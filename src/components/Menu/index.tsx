8017c38463036c4cb303adc0056a6a813a2a33f1
Pepestar <mateusmartinez1@hotmail.com>
2025-05-12 14:46:43 -0300

    atualizando o icone do tema

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,11 +1,15 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react'
+import { HistoryIcon, HouseIcon, MoonIcon, SettingsIcon, SunIcon } from 'lucide-react'
 import styles from './styles.module.css'
 import { useEffect, useState } from 'react'
 
 type AvailableThemes = 'dark' | 'light'
 
 export function Menu() {
-  const [theme, setTheme] = useState<AvailableThemes>('dark')
+  const [theme, setTheme] = useState<AvailableThemes>(() => {
+    const storageTheme =
+      (localStorage.getItem('theme') as AvailableThemes || 'dark')
+    return storageTheme
+  })
 
   function handleThemeChange(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
   ) {
@@ -16,9 +20,15 @@ export function Menu() {
     })
   }
 
+  const nextThemeIcon = {
+    dark: <SunIcon />,
+    light: <MoonIcon />
+  }
+
 
   useEffect(() => {
     document.documentElement.setAttribute('data-theme', theme)
+    localStorage.setItem('theme', theme)
   }, [theme])
 
 
@@ -38,7 +48,7 @@ export function Menu() {
     <a className={styles.menuLink} href="#" aria-label='Alterar tema' title='Alterar tema'
       onClick={handleThemeChange}
     >
-      <SunIcon width={64} />
+      {nextThemeIcon[theme]}
 
     </a>
 
